8ac7483097449105c8b6d7b2215ce89f61c236f8
chinm0y <chinmoy1952@gmail.coom>
2026-08-13 23:55:33 +0600

    update

--- basic/arithmatic_operator.py
+++ basic/arithmatic_operator.py
@@ -3,6 +3,6 @@ print("Enter the day: ")
 
 num = input()
 
-x = "Fri" if num == 5 else "Sat" if num == 6 else "Sun" if num == 7 else "weekday"
+x = "Fri" if num == 7 else "Sat" if num == 1 else "Sun" if num == 2 else "mon" if num == 3 else "tue" if num == 4 else "wed" if num == 5 else "thu"
 
 print(x)
