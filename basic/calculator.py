bebc0618554422e784a3b14f452f4d109d6adbe0
chinm0y <chinmoy1952@gmail.com>
2026-08-14 15:53:42 +0600

    update

---
+++ basic/calculator.py
@@ -0,0 +1,13 @@
+print("\n Calculator:")
+
+num1 = 400
+
+num2 = 300
+
+print(f"Number 1: {num1}, Number 2: {num2}")
+print(f"Sum: {num1 + num2}")
+print(f"Difference: {num1 - num2}")
+print(f"Product: {num1 * num2}")
+print(f"Quotient: {num1 / num2:.2f}")
+print(f"Remainder: {num1 % num2}")
+print(f"Power: {num1} to the power of 2 = {num1 ** 2}")
