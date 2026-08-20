cc4a005859573b0ea32a5020070745e563a79b41
chinm0y <chinmoy1952@gmail.com>
2026-08-20 12:30:33 +0600

    little changes

--- basic/avarage.py
+++ basic/avarage.py
@@ -1,6 +1,6 @@
 print('university cgpa calculator')
 
-marks = [3.0, 3.25, 3.0, 2.9, 3.0, 4.0, 4.0, 4.0]
+marks = [2.9, 2.9, 3.0, 3.2, 3.4, 4.0, 4.0, 4.0]
 average = sum(marks) / len(marks)
 print(f"\nMarks: {marks}")
 print(f"Average: {average:.2f}")
