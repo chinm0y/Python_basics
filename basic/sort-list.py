f0e5771d59c211fa1a5a736eb962cf262861c4a6
chinm0y <chinmoy1952@gmail.com>
2026-08-23 22:38:11 +0600

    sort-list

---
+++ basic/sort-list.py
@@ -0,0 +1,17 @@
+
+
+##numerical
+
+thislist = [100, 50, 65, 82, 23]
+thislist.sort()
+print(thislist)
+
+
+
+
+
+##string
+
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+thislist.sort(reverse = True)
+print(thislist)
