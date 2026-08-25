3991808f922a8c1042d2f8259e084dee3af2e356
chinm0y <chinmoy1952@gmail.com>
2026-08-25 21:20:17 +0600

    tuple-started

---
+++ basic/tuple.py
@@ -0,0 +1,12 @@
+thistuple = ("apple",)
+print(type(thistuple))
+
+#NOT a tuple
+thistuple = ("apple")
+print(type(thistuple))
+
+
+tuple1 = ("apple", "banana", "cherry")
+tuple2 = (1, 5, 7, 9, 3)
+tuple3 = (True, False, False)
+print(tuple1,tuple2,tuple3)
