9b91580f9d3f8de385b26ae8ab9ef061af3f90eb
chinm0y <chinmoy1952@gmail.com>
2026-08-25 21:15:54 +0600

    list completed

---
+++ basic/list-ex.py
@@ -0,0 +1,16 @@
+colors = ["red", "green", "blue"]
+
+# Print the first item
+print(colors[0])
+
+# Change the second item to "yellow"
+colors[1] = "yellow"
+
+# Add "purple" to the end
+colors.append("purple")
+
+# Remove "red"
+colors.remove("red")
+
+# Print the list
+print(colors)
