29fc87b7689a5e4093c535c8e5fb864d83f2e89c
chinm0y <chinmoy1952@gmail.com>
2026-08-26 21:20:01 +0600

    loops  in tuples

---
+++ basic/loops-in-tuples.py
@@ -0,0 +1,12 @@
+thistuple = ("apple", "banana", "chinmoy")
+for x in thistuple:
+ print(x)
+ 
+ 
+ print("===========================")
+ 
+ thistuple = ("apple", "banana", "chinmoy")
+for i in range(len(thistuple)):
+    print(thistuple[i])
+    
+    
