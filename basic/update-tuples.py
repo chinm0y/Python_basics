dea9870db0e5b6ca932db2e542e0e363cea7876f
chinm0y <chinmoy1952@gmail.com>
2026-08-26 20:22:42 +0600

    changes

--- basic/update-tuples.py
+++ basic/update-tuples.py
@@ -19,3 +19,22 @@ thistuple = ("apple", "banana", "cherry")
 y = list(thistuple)
 y.append("orange")
 thistuple = tuple(y)
+
+
+
+
+thistuple = ("apple", "banana", "cherry")
+y = ("orange",)
+thistuple += y
+
+print(thistuple)
+
+
+
+
+
+thistuple = ("apple", "banana", "cherry")
+y = list(thistuple)
+y.remove("apple")
+thistuple = tuple(y)
+print(y)
