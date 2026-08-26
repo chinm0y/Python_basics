6dcdaa83d795fa6579916a137e7673f91d689238
chinm0y <chinmoy1952@gmail.com>
2026-08-26 20:08:38 +0600

    tuple lesson 2

---
+++ basic/access-tuples.py
@@ -0,0 +1,14 @@
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+
+
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
+
--- basic/tuple.py
+++ basic/tuple.py
@@ -10,3 +10,4 @@ tuple1 = ("apple", "banana", "cherry")
 tuple2 = (1, 5, 7, 9, 3)
 tuple3 = (True, False, False)
 print(tuple1,tuple2,tuple3)
+
