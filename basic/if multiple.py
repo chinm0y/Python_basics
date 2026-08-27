dd070c41f76e8d8c6d3f39b72ff49695464fe1d4
chinm0y <chinmoy1952@gmail.com>
2026-08-27 23:30:28 +0600

    if multiple

---
+++ basic/if multiple.py
@@ -0,0 +1,13 @@
+age= 20
+if age >= 18:
+     print("you are an adult ")
+     print("you can vote")
+     print("you have full  legal rights ")
+     
+     
+
+##boolean
+
+is_logged_in = True
+if is_logged_in:
+    print("welcome back!")     
