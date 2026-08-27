f30b41a86f824cc84c7c2ac292caf35f9126aca0
chinm0y <chinmoy1952@gmail.com>
2026-08-27 23:21:37 +0600

    if condition

---
+++ basic/if else.py
@@ -0,0 +1,12 @@
+a=33
+b=200
+if b > a:
+ print("b is bigger fatso......")
+ 
+#identation : we need a space before print statement after ':' statement 
+num=15
+if num >0:
+    print("the number is positive") 
+    
+    
+    
