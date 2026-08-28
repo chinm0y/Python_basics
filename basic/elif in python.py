ae69f85c29742c5577a1a905c0c7acf46cbe6723
chinm0y <chinmoy1952@gmail.com>
2026-08-28 22:27:54 +0600

    elif in python

---
+++ basic/elif in python.py
@@ -0,0 +1,21 @@
+a = 33
+b = 34
+if b>a:
+    print("b is greater than a ")
+elif a == b:
+    print("equals")   
+    
+    
+    
+# multiple elif
+
+score = 75
+if score >= 90:
+    print("grade a")
+elif score >= 80:
+    print("grade b")
+elif score >= 70:
+    print("grade is c")
+elif score >=60:
+    print("grade d")
+    
