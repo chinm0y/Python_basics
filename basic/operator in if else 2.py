e6e6db9cb6ef9653efe6e7cf7c373f4bd7a9fad5
chinm0y <chinmoy1952@gmail.com>
2026-08-29 23:06:55 +0600

    combination of operators

---
+++ basic/operator in if else 2.py
@@ -0,0 +1,7 @@
+age = int(input("enter your age: "))
+is_student = False
+has_discount_code = True
+
+
+if (age < 18 or age> 65) and not is_student or has_discount_code:
+    print("discount applies")
