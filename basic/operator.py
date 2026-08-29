6eeb84a1ca46304b43db8b026ab3f072c791b6a6
chinm0y <chinmoy1952@gmail.com>
2026-08-29 23:02:55 +0600

    operator in if else

---
+++ basic/operator.py
@@ -0,0 +1,13 @@
+a = 200
+b = 33
+c = 500
+if a > b and c>a:
+    print("both condition are true")
+
+
+
+x = 300
+y = 33
+z = 5000
+if x>y or x>z:
+    print("at least one of the conditions is true")
