8007d40f8562d16641513324b379cbfc93c13553
chinm0y <chinmoy1952@gmail.com>
2026-08-29 22:55:20 +0600

    update

--- basic/shorthand  if.py
+++ basic/shorthand  if.py
@@ -9,3 +9,8 @@ a = 300
 b = 330
 print("A") if a>b else print("=") if a == b else print("B")
 
+#default value
+
+username = input()
+display_name = username if username else "guest"
+print("Welcome,", display_name)
