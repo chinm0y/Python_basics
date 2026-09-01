476097b534e2d4d52bf1973a8f047ed74b050169
chinm0y <chinmoy1952@gmail.com>
2026-09-01 20:31:53 +0600

    for loop

---
+++ basic/for loop.py
@@ -0,0 +1,15 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    
+    
+
+
+print("====================")
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
+    if x == "banana":
+        break
+    print(x)
--- basic/while loop.py
+++ basic/while loop.py
@@ -29,4 +29,11 @@ while i < 6:
     
     
     
-    
+print("===================")
+
+i = 1 
+while i < 6:
+    print(i)
+    i +=1
+else:
+    print("i is no longer less than 6")
