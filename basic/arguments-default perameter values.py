6bf2596cb13faee74b5023a4958f48b829fe45ef
chinm0y <chinmoy1952@gmail.com>
2026-09-04 08:29:57 +0600

    keyword arguments

---
+++ basic/arguments-default perameter values.py
@@ -0,0 +1,19 @@
+def my_function(name = "friend"):
+  print("Hello", name)
+
+my_function("Emil")
+my_function("Tobias")
+my_function()
+my_function("Linus")
+
+#2
+print("=========================")
+
+def my_function(country = "Norway"):
+  print("I am from", country)
+
+my_function("Sweden")
+my_function("India")
+my_function()
+my_function("Brazil")
+my_function()
