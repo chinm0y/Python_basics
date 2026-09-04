57b7d16defd90237927d98dfca0db830f9ab785e
chinm0y <chinmoy1952@gmail.com>
2026-09-04 08:22:57 +0600

    arguments

---
+++ basic/arguments.py
@@ -0,0 +1,17 @@
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function(input("Enter the name: "))
+my_function(input("Enter your gf name: "))
+my_function(input("enter your 2nd gf name: "))
+
+#here argument is fname
+print("=======================")
+#now with 2 arguments
+
+
+
+def my_function(fname, lname):
+  print(fname + " " + lname)
+
+my_function("Emil", "Refsnes")
