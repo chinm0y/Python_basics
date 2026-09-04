0ac5fe7abb37e85117bc89ff82884dbc3bdfa89c
chinm0y <chinmoy1952@gmail.com>
2026-09-04 08:16:29 +0600

    functions 2

---
+++ basic/function that returns a value.py
@@ -0,0 +1,12 @@
+def get_greetings():
+    return "Hello from a function"
+    
+messege = get_greetings()
+print(messege)
+
+# using the return value directly
+
+def your_greetings():
+    return "Hello from a function"
+
+print(your_greetings)
